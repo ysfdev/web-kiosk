6de8c1cd169d48f67cb946c4d3a5ccecd1f7ad19
Yeramin Santana <ysfdev@gitbucket.org>
2015-09-08 12:14:33 -0400

    more sources added

--- app/contentCrawler.py
+++ app/contentCrawler.py
@@ -9,30 +9,30 @@ from bs4 import BeautifulSoup
 
 
 news_feeds = []
+news_feeds_desc = []
 sports_feeds = []
 weather_feeds = []
 weather_desc_feeds = []
+tech_feeds = []
+movie_feeds = []
+cybersecurity_feeds = []
 
 
 
-class getFeedsContent:
+class getIndexFeedsContent:
 
 
 
     def __init__(self):
+        print("on")
+        #self.get_news_feeds()
+        #self.get_sports_feeds()
+        #self.get_weather_feeds()
 
-        self.get_news_feeds()
-        self.get_sports_feeds()
-        self.get_weather_feeds()
-
-
-    def __del__(self):
-
-        self.browser.close()
 
     def get_news_feeds(self):
 
-        url = 'http://www.usnews.com/rss/news'
+        url = 'http://rssfeeds.usatoday.com/usatoday-NewsTopStories'
 
         #get news data
 
@@ -44,14 +44,19 @@ class getFeedsContent:
 
             soup = BeautifulSoup(r.text, "xml")
             news_data = soup.findAll('title')
+            feeds_desc = soup.findAll('description')
 
 
         news_feeds.clear()  # clear list before appending to it
+        news_feeds_desc.clear()
 
+        i = 1  # counter
         #get data first 10 headings
-        for news in news_data[1:11]:
+        for news in news_data[2:11]:
 
              news_feeds.append(news.text)
+             news_feeds_desc.append(feeds_desc[i].text)
+             i =+ 1
 
 
 
@@ -71,6 +76,7 @@ class getFeedsContent:
 
             soup = BeautifulSoup(r.text, "xml")
             sports_data = soup.findAll('title')
+
 
         sports_feeds.clear()  # clear list before appending to it
 
@@ -100,10 +106,90 @@ class getFeedsContent:
 
         i = 1  # counter for weather feeds
 
-        #iterate weather data
+        #iterate weather dataDOD
         for feeds in feeds_titles[1:]:
 
              weather_feeds.append(feeds.text)
              weather_desc_feeds.append(feeds_desc[i].text) # feed titles descriptions
              i += 1
+
+class getPage2FeedsContent:
+
+
+    def __init__(self):
+
+        print("on")
+        #self.get_tech_feeds()
+        #self.get_cybersecurity_feeds()
+        #self.get_dod_feeds()
+
+    def get_movie_feeds(self):
+
+        url = 'http://movieweb.com/rss/new-movies/'
+
+        #get news data
+
+        r = requests.get(url)
+
+        #find elements
+
+        if r.status_code == 200:
+
+            soup = BeautifulSoup(r.text, "xml")
+            feeds_data = soup.findAll('title')
+
+
+        movie_feeds.clear()  # clear list before appending to it
+
+        #get data first 10 headings
+        for feeds in feeds_data[1:11]:
+
+            movie_feeds.append(feeds.text)
+
+    def get_tech_feeds(self):
+
+        url = "http://www.cnet.com/rss/news/"
+
+        #get news data
+
+        r = requests.get(url)
+
+        #find elements
+
+        if r.status_code == 200:
+
+            soup = BeautifulSoup(r.text, "xml")
+            feeds_data = soup.findAll('title')
+
+
+        tech_feeds.clear()  # clear list before appending to it
+
+        #get data first 10 headings
+        for feeds in feeds_data[2:12]:
+
+             tech_feeds.append(feeds.text)
+
+    def get_cybersecurity_feeds(self):
+
+        url = 'http://feeds.feedburner.com/TheHackersNews'
+
+
+        #get news data
+
+        r = requests.get(url)
+
+        #find elements
+
+        if r.status_code == 200:
+
+            soup = BeautifulSoup(r.text, "xml")
+            feeds_data = soup.findAll('title')
+
+
+        cybersecurity_feeds.clear()  # clear list before appending to it
+
+        #get data first 10 headings
+        for feeds in feeds_data[1:11]:
+
+            cybersecurity_feeds.append(feeds.text)
 
